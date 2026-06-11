c281f39104f6be8d7118cd24daddb0c9806846b2
Kunal Yadav <.\190859185+Scemer13@users.noreply.github.com>
2026-06-11 14:11:28 +0530

    chapter 4 begininng

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,8 +13,10 @@ pub struct FormData {
 pub async fn subscribe(form: web::Form<FormData>,pool: web::Data<PgPool>) -> HttpResponse {
     // Execute the insertion query
     let request_id = Uuid::new_v4();
-    log::info!("request_id {} - Adding {} {} as a new subscriber", request_id, form.email, form.name);
-    log::info!("Saving new subscriber details in the database");
+    let request_span=tracing::info_span!("Adding a new subscriber.", %request_id, subscriber_email=%form.email, %subscriber_name=%form.name);
+    let _request_span_guard=request_span.enter();
+    tracing::info!("request_id {} - Adding {} {} as a new subscriber", request_id, form.email, form.name);
+    tracing::info!("Saving new subscriber details in the database");
     match sqlx::query!(r#"INSERT INTO subscriptions (id, email, name, subscribed_at)VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
         form.email,
@@ -25,11 +27,11 @@ pub async fn subscribe(form: web::Form<FormData>,pool: web::Data<PgPool>) -> Htt
         .await
     {
         Ok(_) => {
-            log::info!("request_id {} - New subscriber details saved have been database", request_id);
+            tracing::info!("request_id {} - New subscriber details saved have been database", request_id);
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log ::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
+            tracing::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
