90e44ff5d1ed09b1dd209d2e262653a454ea8186
Kunal Yadav <.\190859185+Scemer13@users.noreply.github.com>
2026-06-15 15:37:09 +0530

    chapter 4 complete

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-use zero2prod::run;
-use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
-use sqlx::{PgPool};
-use env_logger::Env;
+use zero2prod::startup::run;
+use sqlx::postgres::PgPool;
+use std::net::TcpListener;
+use tracing::{Subscriber, subscriber::set_global_default};
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use secrecy::{ExposeSecret, Secret};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(),std::io::stdout);
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(
-        &configuration.database.connection_string()
+        &configuration.database.connection_string().expose_secret()
         )
         .await
         .expect("Failed to connect to Postgres");
